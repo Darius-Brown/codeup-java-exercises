2d935e30277e2aa8555d879182ac0ad46b636f5b
Darius Brown <Brown.darius.r@gmail.com>
2021-05-06 09:43:10 -0500

    working on input

---
+++ src/util/InputTest.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.Scanner;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Input input = new Input();
+        System.out.println(input.getString);
+    }
+}

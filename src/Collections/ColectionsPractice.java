c57b5d0a2decde4d6fd7d8964d85d0c6dbe852a1
Darius Brown <Brown.darius.r@gmail.com>
2021-05-07 11:42:02 -0500

    Collections Lecture notes w bonus

---
+++ src/Collections/ColectionsPractice.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//public class ColectionsPractice extends CollectionsLecture{
+//
+//    static String upperLastElement(List<String> lists) {
+//
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        ArrayList<String> namesPrac = new ArrayList<>(Arrays.asList("Justin", "Douglas", "Kenneth"));
+//        System.out.println(printEachElement(namesPrac));
+//
+//    }
+//}

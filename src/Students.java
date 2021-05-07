df78be206a2dae2bd1a90561d147a4c2b9e0d4a8
Darius Brown <Brown.darius.r@gmail.com>
2021-05-07 11:57:01 -0500

    Starting collections exercise

---
+++ src/Students.java
@@ -0,0 +1,27 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Students {
+
+    // returns the student's name
+    public String getName() {
+        return null;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<String> students = new ArrayList<>(Arrays.asList("Mike", "Chris", "Darius", "Jack"));
+        ArrayList<Integer> grades = new ArrayList<>(Arrays.asList());
+    }
+}

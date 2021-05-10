ab03301e27255ff34ec74a53db63b8612f45298e
Darius Brown <Brown.darius.r@gmail.com>
2021-05-10 14:01:20 -0500

    comment changes

--- src/util/Input.java
+++ src/util/Input.java
@@ -7,8 +7,8 @@ public class Input {
     //    create a class named Input that has a private property named scanner
     private Scanner scanner;
 
-    
-    static String getString(String s){
+
+    static String getString(String s) {
         Scanner sc = new Scanner(System.in);
         Integer integer = Integer.valueOf(s);
         Double aDouble = Double.valueOf(s);
@@ -18,52 +18,55 @@ public class Input {
             return input;
         } catch (ArithmeticException e) {
             return String.valueOf(integer);
-        } catch (Exception e){
+        } catch (Exception e) {
             return String.valueOf(aDouble);
 
-    }
-
-    static boolean yesNo(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Type Yes or No");
-        String input = sc.nextLine();
-        if (input.equalsIgnoreCase("yes")|| input.equalsIgnoreCase("y")){
-            return true;
-        } else return false;
-    }
-
-    static int getInt(int min, int max){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number between 0 and 25");
-        int input = sc.nextInt();
-            if (input >= min && input <= max){
-                System.out.println("The entered number is in range");
-            } else return getInt(0,25);
-
-        return input;
-    }
-    static int getInt(){
-        System.out.println("Type any int");
-        Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
-    }
-    static double getDouble(double min, double max){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a decimal number between 0 and 25");
-        double input = sc.nextDouble();
-        if (input >= min && input <= max){
-            System.out.println("The entered number is in range");
-        } else return getDouble(0,25);
-        return input;
-    }
-    static double getDouble(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Type any decimal number");
-        double input = sc.nextDouble();
-        return input;
-    }
-
+            }
+        }
 }
+
+
+//    static boolean yesNo(){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Type Yes or No");
+//        String input = sc.nextLine();
+//        if (input.equalsIgnoreCase("yes")|| input.equalsIgnoreCase("y")){
+//            return true;
+//        } else return false;
+//    }
+//
+//    static int getInt(int min, int max){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number between 0 and 25");
+//        int input = sc.nextInt();
+//            if (input >= min && input <= max){
+//                System.out.println("The entered number is in range");
+//            } else return getInt(0,25);
+//
+//        return input;
+//    }
+//    static int getInt(){
+//        System.out.println("Type any int");
+//        Scanner sc = new Scanner(System.in);
+//        return sc.nextInt();
+//    }
+//    static double getDouble(double min, double max){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a decimal number between 0 and 25");
+//        double input = sc.nextDouble();
+//        if (input >= min && input <= max){
+//            System.out.println("The entered number is in range");
+//        } else return getDouble(0,25);
+//        return input;
+//    }
+//    static double getDouble(){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Type any decimal number");
+//        double input = sc.nextDouble();
+//        return input;
+//    }
+//
+//}
 //
 //
 //    Create an input validation class
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -2,11 +2,11 @@ package util;
 
 public class InputTest {
     public static void main(String[] args) {
-        System.out.println(Input.getString());
-        System.out.println(Input.yesNo());
-        System.out.println(Input.getInt(0,25));
-        System.out.println(Input.getInt());
-        System.out.println(Input.getDouble(0,50));
-        System.out.println(Input.getDouble());
+        System.out.println(Input.getString("6"));
+//        System.out.println(Input.yesNo());
+//        System.out.println(Input.getInt(0,25));
+//        System.out.println(Input.getInt());
+//        System.out.println(Input.getDouble(0,50));
+//        System.out.println(Input.getDouble());
     }
 }
